a1d9670cce005bc1ca170166625429ffb2e33594
Abolfazl Chaman <88617114+abolfazlchaman@users.noreply.github.com>
2025-04-22 01:24:52 +0330

    added shabnam as main and geist as fallback

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -1,23 +1,37 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { ThemeProvider } from "@/components/theme-provider/theme-provider";
-import { LanguageProvider } from "@/contexts/language-context";
-import { CookiesProvider } from "react-cookie";
 import { Providers } from "./provider";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+import localFont from "next/font/local";
 
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
 });
 
+const shabnam = localFont({
+  src: [
+    {
+      path: "../../../public/fonts/Shabnam-Light.woff2",
+      weight: "300",
+      style: "normal",
+    },
+    {
+      path: "../../../public/fonts/Shabnam.woff2",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../../../public/fonts/Shabnam-Bold.woff2",
+      weight: "700",
+      style: "normal",
+    },
+  ],
+  variable: "--font-shabnam",
+  display: "swap",
+});
+
 export const metadata: Metadata = {
-  // spell-checker: disable-next-line
   title: "Abolfazl Chaman",
   description: "My Next.js experimental portfolio",
 };
@@ -33,12 +47,15 @@ export default async function RootLayout({
   children: React.ReactNode;
   params: Promise<{ lang: "en-US" | "fa" }>;
 }>) {
+  const resolvedParams = await params;
+  const dir = resolvedParams.lang === "fa" ? "rtl" : "ltr";
+
   return (
-    <html lang={(await params).lang}>
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        <Providers>
-          {children}
-        </Providers>
+    <html
+      lang={resolvedParams.lang}
+      dir={dir}>
+      <body className={`${shabnam.variable} ${geistMono.variable} antialiased`}>
+        <Providers>{children}</Providers>
       </body>
     </html>
   );

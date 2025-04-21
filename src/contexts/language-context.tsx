058c85ba3adb77e4d95733f57d3979015e094a9a
Abolfazl Chaman <88617114+abolfazlchaman@users.noreply.github.com>
2025-04-22 02:03:54 +0330

    implement dictionary en

--- src/contexts/language-context.tsx
+++ src/contexts/language-context.tsx
@@ -2,20 +2,25 @@
 
 import { createContext, useContext, useEffect, useState } from "react";
 import { useCookies } from "react-cookie";
+import { getDictionary } from "@/app/[lang]/dictionaries";
+import en  from "@/dictionaries/en.json";
 
 type LanguageContextType = {
   language: string;
   setLanguage: (lang: string) => void;
+  dict: Awaited<ReturnType<typeof getDictionary>>;
 };
 
 const LanguageContext = createContext<LanguageContextType>({
   language: "en",
   setLanguage: () => null,
+  dict: en, // Use imported English dictionary as default
 });
 
 export function LanguageProvider({ children }: { children: React.ReactNode }) {
   const [cookies, setCookie] = useCookies(["language"]);
   const [language, setLanguageState] = useState(cookies.language || "en");
+  const [dict, setDict] = useState<Awaited<ReturnType<typeof getDictionary>>>(en); // Initialize with en.json
 
   const setLanguage = (lang: string) => {
     setLanguageState(lang);
@@ -24,10 +29,11 @@ export function LanguageProvider({ children }: { children: React.ReactNode }) {
 
   useEffect(() => {
     document.documentElement.lang = language;
+    getDictionary(language).then(setDict);
   }, [language]);
 
   return (
-    <LanguageContext.Provider value={{ language, setLanguage }}>
+    <LanguageContext.Provider value={{ language, setLanguage, dict }}>
       {children}
     </LanguageContext.Provider>
   );

3aa83efe54feec13c23bac8293eb8cd1b2658822
Abolfazl Chaman <88617114+abolfazlchaman@users.noreply.github.com>
2025-04-22 10:24:11 +0330

    implemented footer and about component

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -1,15 +1,21 @@
-import { Navigation } from "@/components/navigation/navigation"
-import { Hero } from "@/components/hero/hero"
-import { getDictionary } from "./dictionaries"
+import { Navigation } from "@/components/navigation/navigation";
+import { Hero } from "@/components/hero/hero";
+import { getDictionary } from "./dictionaries";
+import About from "@/components/about/about";
+import { Footer } from "@/components/footer/footer";
 
 export default async function Page({ params }: { params: Promise<{ lang: "en" | "fa" }> }) {
-  const { lang } = await params
-  const dict = await getDictionary(lang)
+  const { lang } = await params;
+  const dict = await getDictionary(lang);
 
   return (
     <main>
       <Navigation fullName={dict.developerInfo.fullName} />
-      <Hero />
+      <div className="container md:px-40 lg:px-60 xl:px-80 px-10 min-w-full">
+        <Hero />
+        <About />
+      </div>
+      <Footer />
     </main>
-  )
+  );
 }

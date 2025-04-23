ed54e6230172c5580b943f135ed5457a34ef19cc
Abolfazl Chaman <abolfazlchaman.info@gmail.com>
2025-04-23 23:32:15 +0330

    added types for dictionary

---
+++ src/types/dictionary.d.ts
@@ -0,0 +1,45 @@
+// types/translation.d.ts
+
+export interface TranslationDict {
+  language: string
+  developerInfo: {
+    fullName: string
+    profession: string
+    experience: string
+    location: string
+  }
+  theme: {
+    toggle: string
+    light: string
+    dark: string
+    system: string
+  }
+  navigation: {
+    home: string
+    about: string
+    projects: string
+    blog: string
+    toggle_menu: string
+  }
+  footer: {
+    quote: string
+    author: string
+    copyrightText: string
+  }
+  about: {
+    title: string
+    description: string
+    buttonTextFa: string
+    buttonTextEn: string
+  }
+  projects: {
+    title: string
+    subtitle: string
+    projectsData: {
+      title: string
+      description: string
+      link: string
+      technologies: string[]
+    }[]
+  }
+}

193ddc60f47b2957db0d17a079ef95c4fd03723e
Abolfazl Chaman <abolfazlchaman.info@gmail.com>
2025-04-24 08:38:56 +0330

    removed unused icon used mapping

--- src/components/skills/skills.tsx
+++ src/components/skills/skills.tsx
@@ -1,7 +1,32 @@
 'use client'
 import { useLanguage } from "@/contexts/language-context";
 import { Skill } from "../skill/skill";
-import { SiAmazonwebservices, SiCypress, SiGraphql, SiJavascript, SiJest, SiNextdotjs, SiNodedotjs, SiReact, SiRedux, SiTailwindcss, SiTypescript } from "react-icons/si";
+import {
+  SiAmazonwebservices,
+  SiCypress,
+  SiGraphql,
+  SiJest,
+  SiNextdotjs,
+  SiNodedotjs,
+  SiReact,
+  SiRedux,
+  SiTailwindcss,
+  SiTypescript
+} from "react-icons/si";
+
+const skills = [
+  { name: "TypeScript", icon: <SiTypescript size={40} /> },
+  { name: "AWS", icon: <SiAmazonwebservices size={40} /> },
+  { name: "React", icon: <SiReact size={40} /> },
+  { name: "Next.js", icon: <SiNextdotjs size={40} /> },
+  { name: "Node.js", icon: <SiNodedotjs size={40} /> },
+  { name: "GraphQL", icon: <SiGraphql size={40} /> },
+  { name: "Redux", icon: <SiRedux size={40} /> },
+  { name: "Tailwind CSS", icon: <SiTailwindcss size={40} /> },
+  { name: "Jest", icon: <SiJest size={40} /> },
+  { name: "Cypress", icon: <SiCypress size={40} /> },
+];
+
 export function Skills() {
   const { dict } = useLanguage();
 
@@ -10,51 +35,13 @@ export function Skills() {
       <h2 className="text-3xl font-semibold tracking-tight mb-5 text-center">
         {dict.skills.title}
       </h2>
-      <p className="text-lg text-muted-foreground text-justify mb-10">
-      {dict.skills.sub}
+      <p className="text-lg text-muted-foreground text-justify mb-10 max-w-3xl">
+        {dict.skills.sub}
       </p>
-      <div className="flex flex-wrap justify-center gap-10">
-      <Skill
-          name="TypeScript"
-          icon={<SiTypescript size={40} />}
-        />
-        <Skill
-          name="AWS"
-          icon={<SiAmazonwebservices size={40} />}
-        />
-        <Skill
-          name="React"
-          icon={<SiReact size={40} />}
-        />
-        <Skill
-          name="Next.js"
-          icon={<SiNextdotjs size={40} />}
-        />
-        <Skill
-          name="Node.js"
-          icon={<SiNodedotjs size={40} />}
-        />
-        <Skill
-          name="GraphQL"
-          icon={<SiGraphql size={40} />}
-        />
-        <Skill
-          name="Redux"
-          icon={<SiRedux size={40} />}
-        />
-        <Skill
-          name="Tailwind CSS"
-          icon={<SiTailwindcss size={40} />}
-        />
-        <Skill
-          name="Jest"
-          icon={<SiJest size={40} />}
-        />
-        <Skill
-          name="Cypress"
-          icon={<SiCypress size={40} />}
-        />
-        {/* Add more skills here */}
+      <div className="flex flex-wrap justify-evenly gap-10 max-w-6xl">
+        {skills.map((skill, index) => (
+          <Skill key={index} name={skill.name} icon={skill.icon} />
+        ))}
       </div>
     </section>
   );

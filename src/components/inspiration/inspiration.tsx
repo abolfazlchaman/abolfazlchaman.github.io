49d8fb80efcb0b821da352fb70f77151786d40b8
Abolfazl Chaman <abolfazlchaman.info@gmail.com>
2025-04-24 21:54:56 +0330

    removed unused code

--- src/components/inspiration/inspiration.tsx
+++ src/components/inspiration/inspiration.tsx
@@ -3,43 +3,10 @@
 import React from 'react'
 import Image from 'next/image'
 import { useLanguage } from '@/contexts/language-context'
-import { usePathname } from 'next/navigation'
-import steve from '../../../public/images/Steve_Jobs.jpg'
-import winston from '../../../public/images/churchil.webp'
-import frank from '../../../public/images/roosevelt.jpg'
-type Quote = {
-  text: string
-  author: string
-  image: any
-  wikiLink: string
-  isReversed?: boolean
-}
 
 export default function Inspiration () {
-  const pathname = usePathname()
-  const isFarsi = pathname.startsWith('/fa')
   const { dict } = useLanguage()
-  const quotes: Quote[] = [
-    {
-      text: '“Success is not final, failure is not fatal: It is the courage to continue that counts.”',
-      author: 'Winston Churchill',
-      image: winston,
-      wikiLink: 'https://en.wikipedia.org/wiki/Winston_Churchill'
-    },
-    {
-      text: '“The only way to do great work is to love what you do.”',
-      author: 'Steve Jobs',
-      image: steve,
-      wikiLink: 'https://en.wikipedia.org/wiki/Steve_Jobs',
-      isReversed: true
-    },
-    {
-      text: '“The only limit to our realization of tomorrow is our doubts of today.”',
-      author: 'Franklin D. Roosevelt',
-      image: frank,
-      wikiLink: 'https://en.wikipedia.org/wiki/Franklin_D._Roosevelt'
-    }
-  ]
+
   return (
     <section id='inspiration' className='py-16'>
       <div className='container mx-auto'>
@@ -59,12 +26,12 @@ export default function Inspiration () {
         </p>
 
         <div className='flex flex-col gap-8'>
-          {quotes.map((quote, index) => {
+          {dict.quotes.map((quote, index) => {
             const isReversed = index === 1
             return (
               <div
                 key={index}
-                className={`relative bg-white dark:bg-black/10 p-6 rounded-2xl shadow-[0px_4px_30px_0px_rgba(0,0,0,0.20)] dark:shadow-[0px_4px_30px_0px_rgba(255,255,255,0.10)] flex flex-col sm:flex-row ${
+                className={`relative bg-background p-6 rounded-2xl shadow-[0px_4px_30px_0px_rgba(0,0,0,0.20)] dark:shadow-[0px_4px_30px_0px_rgba(255,255,255,0.10)] flex flex-col sm:flex-row ${
                   isReversed ? 'sm:flex-row-reverse' : ''
                 } gap-6 items-center sm:items-start transition-transform`}
               >
@@ -81,9 +48,9 @@ export default function Inspiration () {
                 </div>
 
                 {/* Text content */}
-                <div className='relative z-10 w-full text-center sm:text-left p-6'>
+                <div className='relative rtl:text-right z-10 w-full text-left sm:text-left p-6'>
                   <a
-                    href={quote.wikiLink}
+                    href={quote.wiki}
                     target='_blank'
                     rel='noopener noreferrer'
                     className='text-foreground hover:underline hover:decoration-0'
@@ -93,7 +60,7 @@ export default function Inspiration () {
                     </p>
                   </a>
                   <blockquote className='text-xl italic text-foreground drop-shadow-lg'>
-                    {quote.text}
+                    {quote.quote}
                   </blockquote>
                 </div>
               </div>
